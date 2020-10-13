42f776f9161b84e2ef1de7961064a653f4175049
vaibhav <rautvaibhav56@gmail.com>
2020-10-13 18:35:18 +0530

    java type casting

---
+++ TypeCasting/src/TypeCasting.java
@@ -0,0 +1,10 @@
+//narrowing (integer-> byte)
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		int a = 300;
+	      byte b = (byte)a; // narrowing
+	      System.out.println(b);
+	}
+
+}
---
+++ TypeCasting/src/TypeCastingWidening.java
@@ -0,0 +1,10 @@
+//TypeCastingWidening (integer -> long)
+public class TypeCastingWidening {
+
+	public static void main(String[] args) {
+		 int a = 1250;
+	      long b = a;
+	      System.out.println(b);
+	}
+
+}
